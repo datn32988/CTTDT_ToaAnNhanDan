40455a81b72e14626e0f67acf1fc774c8fd7c4f2
tonnahe171051 <tonnahe171051@gmail.com>
2026-03-13 10:36:18 +0700

    feature: paging model

---
+++ BE/ToaAnNhanDan.Api/Dtos/Common/Paging.cs
@@ -0,0 +1,9 @@
+namespace ToaAnNhanDan.Api.Dtos.Common
+{
+    public class Paging
+    {
+        public int Page { get; set; }
+        public int? Next { get; set; }
+        public int? Prev { get; set; }
+    }
+}
